f066a5ddad98f6b50e0651ec5872902d3df98b16
yuguangbao <475209990@qq.com>
2019-01-02 17:07:04 +0800

    主文件添加读取日志

--- src/FileReadUtil.java
+++ src/FileReadUtil.java
@@ -5,6 +5,7 @@ public class FileReadUtil {
 
     public static void main(String[] args) {
         File file = new File("E:\\document\\jquery-3.3.1.js");
+        System.out.println("读取文件！");
     }
 
 }
