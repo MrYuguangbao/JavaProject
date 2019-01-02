509b30fe803c19096812283919b7db76a9b86108
yuguangbao <475209990@qq.com>
2019-01-02 16:56:15 +0800

    添加读取的文件路径

--- src/FileReadUtil.java
+++ src/FileReadUtil.java
@@ -1,2 +1,10 @@
+import java.io.File;
+
 public class FileReadUtil {
+    private String name;
+
+    public static void main(String[] args) {
+        File file = new File("E:\\document\\jquery-3.3.1.js");
+    }
+
 }
