9e68366088e161f976c23fb8d2dda45e526325f7
yuguangbao <475209990@qq.com>
2019-01-04 09:36:52 +0800

    从console读入

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Test {
     private String name;
+
+    public static void main(String[] args) {
+
+        System.out.println("input:");
+        InputStream inputStream = System.in;
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        try {
+            String result = bufferedReader.readLine();
+            System.out.println("result:"+result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
