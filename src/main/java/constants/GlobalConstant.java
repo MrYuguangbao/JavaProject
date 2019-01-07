2e9cece0771d141df84da269058d67122900d519
yuguangbao <475209990@qq.com>
2019-01-07 16:35:37 +0800

    全局常量

---
+++ src/main/java/constants/GlobalConstant.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class GlobalConstant {
+
+    public static final String EXCEL_PATH = "C:\\Users\\yuguangbao\\Desktop\\result.xls";
+
+    public static final String RESULT_EXCEL_PATH = "C:\\Users\\yuguangbao\\Desktop\\myexcel.xlsx";
+}
